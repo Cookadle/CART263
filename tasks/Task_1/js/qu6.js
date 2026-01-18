a9400c870bad463fba4b30c1635e4c6dabceb659
Cookadle <jeany.corrius@hotmail.com>
2026-01-18 16:32:52 -0500

    finished que 6

--- tasks/Task_1/js/qu6.js
+++ tasks/Task_1/js/qu6.js
@@ -1,7 +1,7 @@
 "use strict";
 
-const message= "test";
-
+const message= "test"; //text object
+let offset = 20; //spacing variable 
 
 function setup() {
     createCanvas(600, 400);
@@ -10,11 +10,30 @@ function setup() {
 
 
 function draw(){
+background(88, 90, 30);
 
-    background(88, 90, 30);
     textSize(28);
-    fill(255);// white text
-    textAlign(CENTER, CENTER);//text centered
+    fill(255);//white text
+    textAlign(CENTER, CENTER);
+  text(message,300,200);
+
+   for(let i = 0; i<10; i++){
+    const x = i * offset + 15 ; //x posit will space numbers
+    let y=15
+   
+  fill(255);
+        textSize(20);
+        text(i, x, y);
+}
+
+
+     for(let j = 15; j>1; j--){
+    const y = j * offset + 15 ; //yposit will space numbers
+    let x=15
+      fill(255);
+        textSize(20);
+        text(j, x, y);
+    }
 
 
 
