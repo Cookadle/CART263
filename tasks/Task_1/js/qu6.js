91ed940f5458ef377b2341e2d4bfcbd913b9c7ff
Cookadle <jeany.corrius@hotmail.com>
2026-01-18 17:02:23 -0500

    realize 7 was missing for some reason????fixed it and did bonus too

--- tasks/Task_1/js/qu6.js
+++ tasks/Task_1/js/qu6.js
@@ -1,37 +1,40 @@
 "use strict";
 
-const message= "test"; //text object
-let offset = 20; //spacing variable 
+const message = "test"; //text object
+let offset = 22; //spacing numbers
 
 function setup() {
     createCanvas(600, 400);
-    
+
 }
 
 
-function draw(){
-background(88, 90, 30);
-
+function draw() {
+    background(88, 90, 30);
+    push();
     textSize(28);
     fill(255);//white text
     textAlign(CENTER, CENTER);
-  text(message,300,200);
+    text(message, 300, 200);
+    pop();
 
-   for(let i = 0; i<10; i++){
-    const x = i * offset + 15 ; //x posit will space numbers
-    let y=15
-   
-  fill(255);
-        textSize(20);
+
+    textAlign(LEFT, TOP);
+    for (let i = 0; i < 10; i++) {
+        const x = i * offset + 40;
+        const y = 20
+
+        fill(255);
+        textSize(16);
         text(i, x, y);
-}
+    }
 
-
-     for(let j = 15; j>1; j--){
-    const y = j * offset + 15 ; //yposit will space numbers
-    let x=15
-      fill(255);
-        textSize(20);
+    textAlign(LEFT, TOP);
+    for (let j = 15; j >= 1; j--) {
+        const y = (16 - j) * offset + 30;//hated this bonus
+        const x = 15
+        fill(255);
+        textSize(16);
         text(j, x, y);
     }
 
