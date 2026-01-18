b8f9f15a6fafdb7216c91b530610fa5e46d98037
Cookadle <jeany.corrius@hotmail.com>
2026-01-18 15:42:04 -0500

    wasnt following instruction proprely so restarted steps 1

--- tasks/Task_1/js/qu6.js
+++ tasks/Task_1/js/qu6.js
@@ -1,21 +1,21 @@
 "use strict";
 
-
+const message= "test";
 
 
 function setup() {
     createCanvas(600, 400);
+    
+}
+
+
+function draw(){
+
     background(88, 90, 30);
     textSize(28);
     fill(255);// white text
-    textAlign(CENTER, CENTER);// text centered
-    text("test", 300, 200);
-    for (let i = 0; i <= 9; i++) {
-        let x = i * 20;
-        let y = 100;
+    textAlign(CENTER, CENTER);//text centered
 
-        text(i, x, y);
-    }
 
 
 }
