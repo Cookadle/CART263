c04d216ac0818cf3d5496a8da33503dd5c79d018
Cookadle <jeany.corrius@hotmail.com>
2026-01-18 19:37:53 -0500

    step 3 of qu7 done

--- tasks/Task_1/js/qu7.js
+++ tasks/Task_1/js/qu7.js
@@ -1,20 +1,29 @@
 "use strict";
-let circleSize = 30
-let spacing = circleSize * 2; 
+let circleSize = 30 //multiple of 5
+let circleColor;//store random color
 
 function setup() {
-    createCanvas(600, 400);
+    createCanvas(600, 300);
+    circleColor = color(random(255), random(255), random(255));
 
 }
 
 
 function draw() {
     background(40, 80, 90);
-    
-    let spacing = 50;//distance btwen circles( let y=start at 50,loop as long yx smaller canvas h/w,increase)
+
+
+    let spacing = circleSize * 1;//space circle 
+    fill(circleColor);
+    stroke(0);
+
+
+
+    //distance btwen circles( let y=start at 50,loop as long yx smaller canvas h/w,increase)
     for (let y = spacing; y < height; y += spacing) { //rows
         for (let x = spacing; x < width; x += spacing) { //columns
             ellipse(x, y, 30, 30);
+
         }
     }
 }
