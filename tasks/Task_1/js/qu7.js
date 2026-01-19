f2e7287e93d55349214908a5ff3e7bba83846f4a
Cookadle <jeany.corrius@hotmail.com>
2026-01-18 19:26:32 -0500

    sttep 1 done little break before step 2

--- tasks/Task_1/js/qu7.js
+++ tasks/Task_1/js/qu7.js
@@ -1,4 +1,6 @@
 "use strict";
+let circleSize = 30
+let spacing = circleSize * 2; 
 
 function setup() {
     createCanvas(600, 400);
@@ -7,5 +9,12 @@ function setup() {
 
 
 function draw() {
-    background(88, 90, 30);
+    background(40, 80, 90);
+    
+    let spacing = 50;//distance btwen circles( let y=start at 50,loop as long yx smaller canvas h/w,increase)
+    for (let y = spacing; y < height; y += spacing) { //rows
+        for (let x = spacing; x < width; x += spacing) { //columns
+            ellipse(x, y, 30, 30);
+        }
+    }
 }
