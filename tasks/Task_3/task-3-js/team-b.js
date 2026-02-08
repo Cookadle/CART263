ada9e5c6d88881ba5a9b5ac0cf4d47b4df4f617c
Cokadle <jeany.corrius@hotmail.com>
2026-02-07 20:49:02 -0500

    finished task 3 ani B

--- tasks/Task_3/task-3-js/team-b.js
+++ tasks/Task_3/task-3-js/team-b.js
@@ -4,108 +4,109 @@ function setup_B() {
   console.log("in b");
   /**************************************************** */
   //get the buttons
-  activateButtons(`#TEAM_B`, "ani_canvB",aniA,aniB,aniC,aniD);
+  activateButtons(`#TEAM_B`, "ani_canvB", aniA, aniB, aniC, aniD);
 
-//  function aniA(parentCanvas) {
-//     console.log("in ani-A -teamB");
-
-
-//     // square grid
-//     let index = 0
-//     for (let r = 0; r < 2; r++) {
-//       for (let e = 0; e < 2; e++) {
-//         let parent = document.getElementById("ani_canvB_A");
-//         let back = document.createElement("div");
-//         back.classList.add("TEAM_B_backSquare" + index);
-//         back.style.width = `187.5px`;
-//         back.style.height = `187.5px`;
-//         parent.appendChild(back);
-//         index++
-//         console.log(back.classList)
-//         back.style.left = ((r + 1) * 187.5) - 187.5 + "px";
-//         back.style.top = ((e + 1) * 187.5) - 187.5 + "px";
-//       }
-//     }
-
-//     // nested for loop for circles grid
-//     for (let i = 0; i < 14; i++) {
-//       for (let j = 0; j < 14; j++) {
-//         //create a grid cell with a div
-//         let parent = document.getElementById("ani_canvB_A");
-//         let d = document.createElement("div");
-//         d.classList.add("TEAM_B_circle");
-//         d.style.width = `20px`;
-//         d.style.height = `20px`;
-//         parent.appendChild(d);
-//         console.log()
-
-//         d.style.left = (i + 1) * 25 + "px";
-//         d.style.top = (j + 1) * 25 + "px";
-//       }
-//     }
-
-//     let canvas = document.querySelector("#ani_canvB_A")
-//     canvas.addEventListener("click", changeDotColor)
-
-//     function changeDotColor(event) {
-
-//       let bounds = canvas.getBoundingClientRect();
-//       console.log(bounds)
-
-//       let mouseX = event.clientX - bounds.left
-//       let mouseY = event.clientY - bounds.top
+  function aniA(parentCanvas) {
+    console.log("in ani-A -teamB");
 
 
-//       let color = "undefined";
+    //     // square grid
+    //     let index = 0
+    //     for (let r = 0; r < 2; r++) {
+    //       for (let e = 0; e < 2; e++) {
+    //         let parent = document.getElementById("ani_canvB_A");
+    //         let back = document.createElement("div");
+    //         back.classList.add("TEAM_B_backSquare" + index);
+    //         back.style.width = `187.5px`;
+    //         back.style.height = `187.5px`;
+    //         parent.appendChild(back);
+    //         index++
+    //         console.log(back.classList)
+    //         back.style.left = ((r + 1) * 187.5) - 187.5 + "px";
+    //         back.style.top = ((e + 1) * 187.5) - 187.5 + "px";
+    //       }
+    //     }
+
+    //     // nested for loop for circles grid
+    //     for (let i = 0; i < 14; i++) {
+    //       for (let j = 0; j < 14; j++) {
+    //         //create a grid cell with a div
+    //         let parent = document.getElementById("ani_canvB_A");
+    //         let d = document.createElement("div");
+    //         d.classList.add("TEAM_B_circle");
+    //         d.style.width = `20px`;
+    //         d.style.height = `20px`;
+    //         parent.appendChild(d);
+    //         console.log()
+
+    //         d.style.left = (i + 1) * 25 + "px";
+    //         d.style.top = (j + 1) * 25 + "px";
+    //       }
+    //     }
+
+    //     let canvas = document.querySelector("#ani_canvB_A")
+    //     canvas.addEventListener("click", changeDotColor)
+
+    //     function changeDotColor(event) {
+
+    //       let bounds = canvas.getBoundingClientRect();
+    //       console.log(bounds)
+
+    //       let mouseX = event.clientX - bounds.left
+    //       let mouseY = event.clientY - bounds.top
 
 
-//       console.log(mouseX, mouseY, (bounds.width / 2), (bounds.height / 2))
-
-//       if (mouseX < (bounds.width / 2) && mouseY < (bounds.height / 2)) {
-//         color = 0
-//       }
-
-//       else if (mouseX < (bounds.width / 2) && mouseY > (bounds.height / 2)) {
-//         color = 1
-//       }
-
-//       else if (mouseX > (bounds.width / 2) && mouseY < (bounds.height / 2)) {
-//         color = 2
-//       }
-
-//       else if (mouseX > (bounds.width / 2) && mouseY > (bounds.height / 2)) {
-//         color = 3
-//       }
+    //       let color = "undefined";
 
 
-//       for (let i = 0; i < 14; i++) {
-//         for (let j = 0; j < 14; j++) {
-//           //create a grid cell with a div
-//           let parent = document.getElementById("ani_canvB_A");
-//           let d = document.createElement("div");
-//           d.classList.add("TEAM_B_circle" + color);
-//           d.style.width = `20px`;
-//           d.style.height = `20px`;
-//           parent.appendChild(d);
-//           console.log()
+    //       console.log(mouseX, mouseY, (bounds.width / 2), (bounds.height / 2))
 
-//           d.style.left = (i + 1) * 25 + "px";
-//           d.style.top = (j + 1) * 25 + "px";
-//         }
-//       }
+    //       if (mouseX < (bounds.width / 2) && mouseY < (bounds.height / 2)) {
+    //         color = 0
+    //       }
+
+    //       else if (mouseX < (bounds.width / 2) && mouseY > (bounds.height / 2)) {
+    //         color = 1
+    //       }
+
+    //       else if (mouseX > (bounds.width / 2) && mouseY < (bounds.height / 2)) {
+    //         color = 2
+    //       }
+
+    //       else if (mouseX > (bounds.width / 2) && mouseY > (bounds.height / 2)) {
+    //         color = 3
+    //       }
 
 
-//     }
-//   }
+    //       for (let i = 0; i < 14; i++) {
+    //         for (let j = 0; j < 14; j++) {
+    //           //create a grid cell with a div
+    //           let parent = document.getElementById("ani_canvB_A");
+    //           let d = document.createElement("div");
+    //           d.classList.add("TEAM_B_circle" + color);
+    //           d.style.width = `20px`;
+    //           d.style.height = `20px`;
+    //           parent.appendChild(d);
+    //           console.log()
+
+    //           d.style.left = (i + 1) * 25 + "px";
+    //           d.style.top = (j + 1) * 25 + "px";
+    //         }
+    //       }
+
+
+    //     }
+  }
 
 
   /****************ANI B ************************************ */
   /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN B INSIDE HERE */
   /****************ANI B ************************************ */
+  //https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
   /**************** TASK *******************************************
    * YOU CAN USE ALL NOTES --- and see my examples in team-h.js for inspiration and possibly help:).
    * 1: create a creatve, visual pattern using text, divs as shapes, images ... draw bunch of squares of differents sizes across the canvas
-   * 2: add in mouseover event listener(s) somewhere to make the sketch interactive... change colour of squares when mouse is over (maybe all squares change colour randomly )
+   * 2: add in mouseover event listener(s) somewhere to make the sketch interactive...add more squares when mouse is over (maybe all squares change colour randomly )
    * or creat a trail of squares random color with the mouse using the mouse as a brush
    * NOTE::: PLEASE::: if you add any custom css PLEASE use the style.css and prefix any class names with your team label
    * i.e. you want to create a custom div class and you are in "Team_A" then call your class TEAM_A_ANI_A_Div -
@@ -116,8 +117,58 @@ function setup_B() {
    * **/
 
   function aniB(parentCanvas) {
-      console.log("in ani-B -teamB");
-    
+    console.log("in ani-B -teamB");
+
+    // Set canvas bag
+    parentCanvas.style.backgroundColor = "rgb(177, 194, 231)";
+    parentCanvas.style.position = "relative"; //to emile/dyna: maybe the circle issues with the first one was because we use absolute instead of relative?
+
+    //random number ( this is what pick square size+random position+colour )
+    function random(min, max) {
+      return Math.random() * (max - min) + min;
+    }
+
+
+
+
+    //create a square div 
+    function createSquare(colorClass) {
+      const square = document.createElement("div");
+      square.className = colorClass; //choose a color from palette beore
+
+      //assign random width and height to square
+      const size = random(20, 60);
+      square.style.width = size + "px";
+      square.style.height = size + "px";
+
+      //random position inside the container aka the parent
+      const x = random(0, parentCanvas.clientWidth - size);//so no overflow out canvas but could remove for more chaoctic feel
+      const y = random(0, parentCanvas.clientHeight - size);
+      square.style.left = x + "px";
+      square.style.top = y + "px";
+
+      parentCanvas.appendChild(square);
+    }
+
+    //draw random squares using colour classes from first exercise
+    function drawRandomSquares(amount) {
+      const colorClasses = [
+        "TEAM_B_backSquare0",
+        "TEAM_B_backSquare1",
+        "TEAM_B_backSquare2",
+        "TEAM_B_backSquare3",
+      ];
+      //loop  to create multiple squares.
+      for (let i = 0; i < amount; i++) {
+        const colorClass = colorClasses[Math.floor(random(0, colorClasses.length))];//random pick colour
+        createSquare(colorClass);
+      }
+    }
+
+    //mouseover will be doing random squares
+    parentCanvas.addEventListener("mouseover", function () {
+      drawRandomSquares(10);//add 10 new square each time
+    });
   }
   /****************ANI C ************************************ */
   /** PUT ALL YOUR CODE FOR INTERACTIVE PATTERN C INSIDE HERE */
@@ -140,7 +191,7 @@ function setup_B() {
    */
 
   function aniC(parentCanvas) {
-      console.log("in ani-C -teamB");
+    console.log("in ani-C -teamB");
 
     /*** THIS IS THE CALLBACK FOR KEY DOWN (* DO NOT CHANGE THE NAME *..) */
     windowKeyDownRef = function (e) {
@@ -152,7 +203,7 @@ function setup_B() {
     /*** THIS IS THE CALLBACK FOR KEY UP (*DO NOT CHANGE THE NAME..) */
     windowKeyUpRef = function (e) {
       console.log(e);
-        console.log("b-up");
+      console.log("b-up");
     };
     //DO NOT REMOVE
     window.addEventListener("keydown", windowKeyDownRef);
@@ -174,7 +225,7 @@ function setup_B() {
    * remember you can define other functions inside....
    * Do not change any code above or the HTML markup.
    * **/
-   function aniD(parentCanvas) {
+  function aniD(parentCanvas) {
     console.log("in ani-D -teamB");
-    }
+  }
 }
