11ac037cb2e2ba8e7fe69e4e28f0fe6dba10467b
Cookadle <jeany.corrius@hotmail.com>
2026-02-22 12:22:44 -0500

    added 1-2-3-4 parts that were done by teammates

--- tasks/Task_4/js/beehive.js
+++ tasks/Task_4/js/beehive.js
@@ -1,22 +1,54 @@
-class beehive {
+class BeeHive {
+  constructor(x, y, size, color) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+    this.height = Math.round(size * 1.35);
+    this.color = color;
+    this.beehiveDiv = document.createElement("div");
+    this.stripes = [];
+    this.holeDiv = document.createElement("div");
+    this.rotation = 0; // actual hive rotation
+    this.angle = 1; // x in sine function
+    this.beeCounter = 0;
+  }
 
-constructor(x,y,width,height,r,g,b){
-this.x=x;
-this.y=y;
-this.width=width;
-this.height=height;
-this.r=r;
-this.g=g;
-this.b=b;
-this.rs=10
-this.beehive=document.createElement("div")
-}
-}
-renderbeehive(){
+  rotateHives() {
+    this.rotation = Math.sin(this.angle) * 10; // last multiplier = amplitude
+    this.angle += 0.02; // rythm
 
-this.beehive.style.width=this.width +"px"
-this.beehive.style.height=this.height +"px"
-this.beehive.style.left=this.left.x +"px"
-this.beehive.style.topt=this.right.y +"px"
-document.getElementsByClassName("sky")[0].appendChild(this.beehive);
+    this.beehiveDiv.style.rotate = this.rotation + "deg"; //updates the rotation of the hives
+  }
+
+  renderBeeHive() {
+    this.beehiveDiv.classList.add("beehive");
+    this.beehiveDiv.style.width = this.size + "px";
+    this.beehiveDiv.style.height = this.height + "px";
+    this.beehiveDiv.style.left = this.x + "px";
+    this.beehiveDiv.style.top = this.y + "px";
+    this.beehiveDiv.style.background = `rgb(${this.color.r},${this.color.g},${this.color.b})`;
+    this.beehiveDiv.style.rotate = this.rotation + "deg";
+
+    this.beehiveDiv.innerHTML = "";
+    this.stripes = [];
+    const stripeCount = 5;
+    for (let i = 0; i < stripeCount; i++) {
+      const stripe = document.createElement("div");
+      stripe.classList.add("beehive__stripe");
+      stripe.style.top = i * (this.height / stripeCount) + "px";
+      this.beehiveDiv.appendChild(stripe);
+      this.stripes.push(stripe);
+    }
+
+    this.holeDiv.innerHTML = `<h3>${this.beeCounter}</h3>`;
+
+    this.holeDiv.classList.add("beehive__hole");
+    this.beehiveDiv.appendChild(this.holeDiv);
+
+    const grass = document.querySelector(".grass");
+    const sky = document.querySelector(".sky");
+    (sky || grass)?.appendChild(this.beehiveDiv);
+  }
+
+
 }
