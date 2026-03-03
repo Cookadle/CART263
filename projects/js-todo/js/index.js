ab846415d6694dc2e75c782a3c410a67a85b28a5
Cookadle <jeany.corrius@hotmail.com>
2026-03-03 15:15:43 -0500

    started adding basic const/variable

---
+++ projects/js-todo/js/index.js
@@ -0,0 +1,4 @@
+//Grab key elements by IDs so we can use
+const taskInput = document.getElementById("task-input");//for adding task box
+const addTaskBtn = document.getElementById("add-task-btn");//for adding the task into the html
+const taskList = document.getElementById("task-list"); //will create a task list that I can fetch
