66f44cf60b07c005f1abd590cabacb6a017e0c5a
Cokadle <jeany.corrius@hotmail.com>
2026-03-07 12:29:39 -0500

    Added variables already called in html.started the add task function with an event handler ,so far nothing works lmao lets see

---
+++ projects/js-todo/js/index.js
@@ -0,0 +1,21 @@
+//referencing the elements by their id that i had set up in the html and store variables here
+const taskInput = document.getElementById("task-input");
+const addTaskBtn = document.getElementById("add-task-btn");
+const taskList = document.getElementById("task-list");
+
+
+//CORE FEATURE STARTS HERE ADDING TASKS BABEY()
+function addTask(){
+
+   const taskText = taskInput.value.trim();//remove extra space in text +read text
+   if (taskText === "") return;//prevent empty tasks
+
+   const li= document.createElement("li")//creating the task list
+   li.className = "task";//for css styling
+   li.textContent = taskText; //
+
+//calling an event listener here for that whe you click task its complete and click again will undo the completed state
+li.addEventListener("click",()=>{
+li.classList.toggle("completed");
+})
+}
