d6be69ff41a8c9fede565ac2cddd3a10f78dd475
Cookadle <jeany.corrius@hotmail.com>
2026-04-07 15:19:44 -0400

    merge branch +adding left over exercise from class of 30 march

--- Class_work_ressources/cart263_24march/js/shadows.js
+++ Class_work_ressources/cart263_24march/js/shadows.js
@@ -6,13 +6,31 @@ const scene = new THREE.Scene();
 const canvas = document.querySelector("canvas#three-ex");
 
 //Ambient Light
-const ambientLight = new THREE.AmbientLight(0xffffff, 1);
-scene.add(ambientLight);
+
+const ambientLight = new THREE.AmbientLight(0xffffff, .25);
 
 // Directional light
-const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+//const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+const directionalLight = new THREE.DirectionalLight(0xffffff, .25);
 directionalLight.position.set(2, 2, -1);
 scene.add(directionalLight)
+//directionalLight.castShadow = true
+
+directionalLight.shadow.mapSize.width = 1024
+directionalLight.shadow.mapSize.height = 1024
+directionalLight.shadow.radius = 10
+
+
+const spotLight = new THREE.SpotLight(0xff0000 ,5, 10, Math.PI * 0.3)
+//new
+spotLight.castShadow = true
+
+spotLight.shadow.mapSize.width = 1024
+spotLight.shadow.mapSize.height = 1024
+spotLight.position.set(0, 2, 2)
+scene.add(spotLight)
+scene.add(spotLight.target)
+
 
 //Sphere and plane
 const geometry = new THREE.SphereGeometry(0.5, 32, 32);
@@ -26,16 +44,20 @@ const plane = new THREE.Mesh(new THREE.PlaneGeometry(5, 5), material);
 scene.add(plane);
 plane.rotation.x = -Math.PI * 0.5;
 plane.position.y = -0.5;
+sphere.castShadow = true
+plane.receiveShadow = true
+
+
 
 const sizes = {
-  width: window.innerWidth,
-  height: window.innerHeight,
+    width: window.innerWidth,
+    height: window.innerHeight,
 };
 const camera = new THREE.PerspectiveCamera(
-  75,
-  sizes.width / sizes.height,
-  0.1,
-  100,
+    75,
+    sizes.width / sizes.height,
+    0.1,
+    100,
 );
 camera.position.x = 1;
 camera.position.y = 1;
@@ -49,16 +71,22 @@ controls.enableDamping = true;
 
 //RENDER
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
+    canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+
+
+renderer.shadowMap.enabled = true
+renderer.shadowMap.type = THREE.PCFSoftShadowMap
+
+console.log(directionalLight.shadow)
 
 
 //ANIMATE
 window.requestAnimationFrame(animate);
 function animate() {
-  controls.update();
+    controls.update();
 
-  renderer.render(scene, camera);
-  window.requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+    window.requestAnimationFrame(animate);
 }
